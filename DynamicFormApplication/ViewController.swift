c1353f5cefb03c51e00aa079377b136761ae0934
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2018-08-10 22:44:14 +0200

    Initial Commit

---
+++ DynamicFormApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicFormApplication
+//
+//  Created by Kamil Tustanowski on 10.08.2018.
+//  Copyright © 2018 Cornerbit Kamil Tustanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
